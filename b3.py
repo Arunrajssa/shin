07a3da62a687761a70c0362caf72549a3b91a45d
Arunrajssa <36511006+Arunrajssa@users.noreply.github.com>
2018-03-01 21:26:33 +0530

    b3.py
    codings.....................

---
+++ b3.py
@@ -0,0 +1,14 @@
+a=[]
+i=0
+n=(int(input("enter the no of elements")))
+for i in range(i,n):
+  b=input("enter number")
+  a.append(b)
+print(a)
+i=0
+for i in range(i,n):
+    if(a[i]==i):
+        print(a[i])
+   
+print('no values like that...index=gn. number')
+
